1dffe542cd3d3a51b848c4e869e81d0ca9e1b744
Rokas <rookas.rudzenskas@gmail.com>
2022-09-07 16:57:54 +0300

    Rokas

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,10 @@
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: 'https://countries.trevorblades.com/',
+    uri: 'https://saltillo.stepzen.net/api/sartorial-opossum/__graphql',
+    headers: {
+        Authorization: `Apikey ${process.env.NEXT_PUBLIC_STEPZEN_KEY}`,
+    },
     cache: new InMemoryCache()
 });
 
