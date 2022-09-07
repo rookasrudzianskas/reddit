46217fb90f8354a17b555275b4c4bf3e27cb513e
Rokas <rookas.rudzenskas@gmail.com>
2022-09-07 14:48:43 +0300

    Rokas

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  // next.config.js
+  images: {
+    domains: ['assets.example.com', 'links.papareact.com', 'logos-world.net'],
+  },
 }
+
