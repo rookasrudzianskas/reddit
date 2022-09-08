8d9cdcebd4930f4bb577356278466a84a2b4b987
Rokas <rookas.rudzenskas@gmail.com>
2022-09-08 15:53:29 +0300

    Rokas

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useQuery} from "@apollo/client";
 import {GET_ALL_POSTS} from "../graphql/queries";
+import Post from './Post';
 
 const Feed = () => {
     const { data, error } = useQuery(GET_ALL_POSTS);
@@ -8,7 +9,10 @@ const Feed = () => {
     const posts: Post[] = data?.getPostList;
     return (
         <div>
-            Hello
+            {posts?.map(post => (
+                // @ts-ignore
+                <Post key={post.id} post={post} />
+                ))}
         </div>
     );
 };
---
+++ components/Post.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+type Props = {
+    post: Post;
+}
+
+const Post = ({post}: Props) => {
+    return (
+        <div>
+            <div>
+
+            </div>
+            {/* rest */}
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Post;
