215d659141c30901b90b021b0ecb2e03519ed8bc
Rokas <rookas.rudzenskas@gmail.com>
2022-09-08 15:22:37 +0300

    Rokas

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import {useQuery} from "@apollo/client";
+import {GET_ALL_POSTS} from "../graphql/queries";
 
 const Feed = () => {
+    const { data, error } = useQuery(GET_ALL_POSTS);
+
+    const posts: Post[] = data?.getPostList;
     return (
         <div>
-
+            Hello
         </div>
     );
 };
--- graphql/queries.ts
+++ graphql/queries.ts
@@ -20,6 +20,25 @@ export const GET_ALL_POSTS = gql`
             title
             subreddit_id
             username
+            comments {
+                created_at
+                id
+                post_id
+                text
+                username
+            }
+            subreddit {
+                created_at
+                id
+                topic
+            }
+            votes {
+                created_at
+                id
+                post_id
+                upvote
+                username
+            }
         }   
     }
 `
