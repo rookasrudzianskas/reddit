469bdcbcff550cd072fd8c7485f2686ef12f813a
Rokas <rookas.rudzenskas@gmail.com>
2022-09-08 20:13:19 +0300

    Rokas

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useQuery} from "@apollo/client";
-import {GET_ALL_POSTS} from "../graphql/queries";
+import {GET_ALL_POSTS, GET_ALL_POSTS_BY_TOPIC} from "../graphql/queries";
 import Post from './Post';
 
 type Props = {
@@ -8,9 +8,13 @@ type Props = {
 }
 
 const Feed = ({topic}: Props) => {
-    const { data, error } = !topic ? useQuery(GET_ALL_POSTS) : useQuery();
+    const { data, error } = !topic ? useQuery(GET_ALL_POSTS) : useQuery(GET_ALL_POSTS_BY_TOPIC, {
+        variables: {
+            topic: topic
+        }
+    });
 
-    const posts: Post[] = data?.getPostList;
+    const posts: Post[] = !topic ? data?.getPostList : data?.getPostListByTopic;
     return (
         <div className="mt-5 space-y-4">
             {posts?.map(post => (
