ecc12b017d12b7a94d8a0c3e9004d5e43e92d52d
Rokas <rookas.rudzenskas@gmail.com>
2022-09-08 20:19:23 +0300

    Rokas

--- components/Post.tsx
+++ components/Post.tsx
@@ -10,6 +10,7 @@ import {
 } from "@heroicons/react/outline";
 import Avatar from "./Avatar";
 import TimeAgo from 'react-timeago';
+import Link from "next/link";
 
 type Props = {
     post: Post;
@@ -17,58 +18,62 @@ type Props = {
 
 const Post = ({post}: Props) => {
     return (
-        <div className="flex cursor-pointer rounded-md border border-gray-300 bg-white shadow-sm hover:border-gray-600 duration-150">
-            <div className="flex flex-col items-center justify-start space-y-1 rounded-l-md bg-gray-50 p-4 text-gray-400">
-                <ArrowUpIcon className="voteButtons hover:text-red-400 duration-150 cursor-pointer" />
-                <p className="text-black text-xs font-bold">0</p>
-                <ArrowDownIcon className="voteButtons hover:text-blue-400 duration-150 cursor-pointer" />
+        <Link href={`/post/${post.id}`}>
+            <div className="flex cursor-pointer rounded-md border border-gray-300 bg-white shadow-sm hover:border-gray-600 duration-150">
+                <div className="flex flex-col items-center justify-start space-y-1 rounded-l-md bg-gray-50 p-4 text-gray-400">
+                    <ArrowUpIcon className="voteButtons hover:text-red-400 duration-150 cursor-pointer" />
+                    <p className="text-black text-xs font-bold">0</p>
+                    <ArrowDownIcon className="voteButtons hover:text-blue-400 duration-150 cursor-pointer" />
+                </div>
+                {/* rest */}
+                <div className="p-3 pb-1">
+                    {/* Header */}
+                    <div className="flex items-center space-x-2">
+                        <Avatar seed={post.subreddit[0]?.topic} />
+                        <p className="text-xs text-gray-400">
+                            <Link href={`/subreddit/${post.subreddit[0]?.topic}`}>
+                                <span className="font-bold text-black hover:text-blue-400 hover:underline">r/{post.subreddit[0]?.topic}</span>
+                            </Link>
+                            • Posted by u/{post.username} <TimeAgo date={post.created_at} />
+                        </p>
+                    </div>
+                    {/*    Body */}
+                    <div className="py-4">
+                        <h2 className="text-xs font-semibold">{post.title}</h2>
+                        <p className="text-sm mt-2 font-light">{post.body}</p>
+                    </div>
+                    {/*    Image*/}
+                    <img src={post.image} className="w-full" alt=""/>
+                    {/*    Goooter*/}
+                    <div className="flex space-x-4 text-gray-400 mt-3">
+                        <div className="postButtons">
+                            <ChatAltIcon className="h-6 w-6" />
+                            <p className="">{post.comments.length} Comments</p>
+                        </div>
+
+                        <div className="postButtons">
+                            <GiftIcon className="h-6 w-6" />
+                            <p className="hidden sm:inline">Award</p>
+                        </div>
+
+                        <div className="postButtons">
+                            <ShareIcon className="h-6 w-6" />
+                            <p className="hidden sm:inline">Share</p>
+                        </div>
+
+                        <div className="postButtons">
+                            <BookmarkIcon className="h-6 w-6" />
+                            <p className="hidden sm:inline">Save</p>
+                        </div>
+
+                        <div className="postButtons">
+                            <DotsHorizontalIcon className="h-6 w-6" />
+                        </div>
+
+                    </div>
+                </div>
             </div>
-            {/* rest */}
-            <div className="p-3 pb-1">
-            {/* Header */}
-                <div className="flex items-center space-x-2">
-                    <Avatar seed={post.subreddit[0]?.topic} />
-                    <p className="text-xs text-gray-400">
-                        <span className="font-bold text-black hover:text-blue-400 hover:underline">r/{post.subreddit[0]?.topic}</span>
-                        • Posted by u/{post.username} <TimeAgo date={post.created_at} />
-                    </p>
-                </div>
-            {/*    Body */}
-                <div className="py-4">
-                    <h2 className="text-xs font-semibold">{post.title}</h2>
-                    <p className="text-sm mt-2 font-light">{post.body}</p>
-                </div>
-            {/*    Image*/}
-                <img src={post.image} className="w-full" alt=""/>
-            {/*    Goooter*/}
-                <div className="flex space-x-4 text-gray-400 mt-3">
-                    <div className="postButtons">
-                        <ChatAltIcon className="h-6 w-6" />
-                        <p className="">{post.comments.length} Comments</p>
-                    </div>
-
-                    <div className="postButtons">
-                        <GiftIcon className="h-6 w-6" />
-                        <p className="hidden sm:inline">Award</p>
-                    </div>
-
-                    <div className="postButtons">
-                        <ShareIcon className="h-6 w-6" />
-                        <p className="hidden sm:inline">Share</p>
-                    </div>
-
-                    <div className="postButtons">
-                        <BookmarkIcon className="h-6 w-6" />
-                        <p className="hidden sm:inline">Save</p>
-                    </div>
-
-                    <div className="postButtons">
-                        <DotsHorizontalIcon className="h-6 w-6" />
-                    </div>
-
-                </div>
-            </div>
-        </div>
+        </Link>
     );
 };
 
