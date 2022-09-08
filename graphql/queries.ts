69e30c73633b43056c94949d97dbe71e0264a4c2
Rokas <rookas.rudzenskas@gmail.com>
2022-09-08 20:11:26 +0300

    Rokas

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -1,5 +1,39 @@
 import {gql} from "@apollo/client";
 
+
+export const GET_ALL_POSTS_BY_TOPIC = gql`
+ query MyQuery($topic: String!) {
+     getPostListByTopic(topic: $topic) {
+         body
+         comments {
+             created_at
+             id
+             post_id
+             text
+             username
+         }
+         created_at
+         id
+         image
+         subreddit {
+             created_at
+             id
+             topic
+         }
+         title
+         subreddit_id
+         username
+         votes {
+             created_at
+             id
+             post_id
+             upvote
+             username
+         }
+     }
+ }
+`
+
 export const GET_SUBREDDIT_BY_TOPIC = gql`
     query MyQuery($topic: String!) {
         getSubredditListByTopic(topic: $topic) {
