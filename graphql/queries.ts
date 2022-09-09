ea5a6d1d3a368fcb1d7a6f4d1b9016237dc6f9f1
Rokas <rookas.rudzenskas@gmail.com>
2022-09-09 12:22:51 +0300

    Rokas

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -13,6 +13,16 @@ export const GET_ALL_VOTES_BY_POST_ID = gql`
     }
 `
 
+export const GET_SUBREDDITS_WITH_LIMIT = gql`
+    query MyQuery($limit: Int!) {
+        getSubredditListLimit(limit: $limit) {
+            created_at
+            id
+            topic
+        }
+    }
+`
+
 
 export const GET_POST_BY_POST_ID = gql`
     query MyQuery($post_id: ID!) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,19 @@ import Image from 'next/image'
 import Header from "../components/Header";
 import PostBox from '../components/PostBox';
 import Feed from "../components/Feed";
+import {GET_SUBREDDITS_WITH_LIMIT} from "../graphql/queries";
+import {useQuery} from "@apollo/client";
+import SubredditRow from "../components/SubredditRow";
 
 const Home: NextPage = () => {
+    const {data} = useQuery(GET_SUBREDDITS_WITH_LIMIT, {
+        variables: {
+            limit: 10
+        }
+    })
+
+    const subreddits: Subreddit[] = data?.getSubredditListLimit;
+
   return (
     <div className="max-w-5xl my-7 mx-auto">
       <Head>
@@ -16,6 +27,15 @@ const Home: NextPage = () => {
         <PostBox />
         <div className="flex">
             <Feed />
+
+            <div className="sticky top-36 mx-5 mt-5 hidden h-fit min-w-[300px] rounded-md border border-gray-300 bg-white lg:inline">
+                <p className="text-md mb-1 p-4 pb-3 font-bold">Top Communities</p>
+                <div>
+                    {subreddits?.map((subreddit, index) =>  (
+                        <SubredditRow key={index} topic={subreddit.topic} index={index} />
+                    ))}
+                </div>
+            </div>
         {/*    Feed */}
         </div>
 
